2eb81f4f9ff251c2ae50f086d5ccf2108f1dd8e0
6Bcoder <111550172+6Bcoder@users.noreply.github.com>
2025-01-11 01:16:20 +0530

    test function for shorten function

--- main_test.go
+++ main_test.go
@@ -40,3 +40,33 @@ func incrementRequestCount(longURL string) {
 	fmt.Println("Parsed domain:", parsedURL.Host)
 	domainVisitCounts[parsedURL.Host]++
 }
+func TestShorten(t *testing.T) {
+	setupMockDB()
+	defer closeMockDB()
+	mock.ExpectQuery("SELECT shortURL FROM UrlRecord WHERE longURL = ?").
+		WithArgs("https://example.com").
+		WillReturnRows(sqlmock.NewRows([]string{"shortURL"}).AddRow("abc123"))
+	shortURL, err := Shorten("https://example.com")
+	assert.NoError(t, err)
+	assert.Equal(t, "abc123", shortURL)
+	incrementRequestCount("https://example.com")
+
+	mock.ExpectQuery("SELECT shortURL FROM UrlRecord WHERE longURL = ?").
+		WithArgs("https://newsite.com").
+		WillReturnRows(sqlmock.NewRows([]string{}))
+
+	mock.ExpectQuery("SELECT shortURL FROM UrlRecord WHERE shortURL = ?").
+		WillReturnRows(sqlmock.NewRows([]string{}))
+
+	mock.ExpectExec("INSERT INTO UrlRecord").
+		WithArgs("https://newsite.com", sqlmock.AnyArg()).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	shortURL, err = Shorten("https://newsite.com")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, shortURL)
+	incrementRequestCount("https://newsite.com")
+	fmt.Println("Domain Counts:", domainVisitCounts)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled expectations: %s", err)
+	}
+}
